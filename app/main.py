20e261acefb6beed6557a93f73a2ea686a672037
Utsav Goyal <goyalutsav2004@gmail.com>
2025-09-08 01:35:33 +0530

    taking input command

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-import sys
+import sys # importing sys module for system specific functions
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    sys.stdout.write("$ ") # this is diff from normal print as it does not print the newline at end
 
-    # Wait for user input
-    input()
+    command = input() # this takes a command as an input 
+
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
