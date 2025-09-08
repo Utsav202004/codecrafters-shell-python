1406a506dc1a9c303a6d9296a0780269dc4e9002
Utsav Goyal <goyalutsav2004@gmail.com>
2025-09-08 17:45:45 +0530

    adding builtin echo

--- app/main.py
+++ app/main.py
@@ -8,9 +8,20 @@ def main():
         sys.stdout.write("$ ") # diff from print, do not print \n (newline)
         command = input() # taking and storing the input
 
-        if command.split()[0] == 'exit':
-            return 0
-        print(f"{command}: command not found") 
+        parts = command.split()
+
+        if not parts:
+            continue # handling empty inputs
+
+
+        if command == 'exit 0':
+            break
+
+        elif parts[0] == 'echo': # builtin echo
+            print(" ".join(parts[1:]))
+
+        else:
+            print(f"{command}: command not found") 
 
 
 if __name__ == "__main__":
