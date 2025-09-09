ea79386cd3764a5a414f8b1f249a8303321c3d68
Utsav Goyal <goyalutsav2004@gmail.com>
2025-09-09 10:07:52 +0530

    Implemented the pwd builtin command

--- app/main.py
+++ app/main.py
@@ -1,50 +1,51 @@
-import sys # importing sys module for system specific functions
+import sys      # importing sys module for system specific functions
 import os
 
-def searcher(type): # searching for a executbale file in the PATH
-    path_string = os.environ.get('PATH', '') # getting the path string
-    dir_list = path_string.split(':') # list of directories
+def searcher(type):     # searching for a executbale file in the PATH
+    path_string = os.environ.get('PATH', '')    # getting the path string
+    dir_list = path_string.split(':')   # list of directories
 
-    for dir in dir_list: # searching
+    for dir in dir_list:    # searching
         full_path = os.path.join(dir, type)
-        if os.path.exists(full_path) and os.access(full_path, os.X_OK): # checking for the existence and access
+        if os.path.exists(full_path) and os.access(full_path, os.X_OK):     # checking for the existence and access
                 return full_path
 
-    return None # if nothing found 
+    return None     # if nothing found 
 
 commands = {
     'exit' : lambda exit_code : os._exit(int(exit_code)),
     'echo' : lambda *args : print(" ".join(args)),
     'type' : lambda type : print(f"{type} is a shell builtin") if (type in commands) else ( print(f"{type} is {path}") if (path := searcher(type)) else print(f"{type}: not found")),
+    'pwd' : lambda : print(os.getcwd()),
 }
 
 def main():
 
-    while True: # creating a REPL
+    while True:     # creating a REPL
 
-        sys.stdout.write("$ ")  # diff from print, does not print \n (newline)
+        sys.stdout.write("$ ")     # diff from print, does not print \n (newline)
 
-        command_with_arg = input().split() # taking and storing the command and arguments as list
+        command_with_arg = input().split()      # taking and storing the command and arguments as list
 
-        command = command_with_arg[0] # the command 
+        command = command_with_arg[0]   # the command 
 
-        if command in commands: # execute builtins
-            commands[command](*command_with_arg[1:]) # passing the argument to the required command function
+        if command in commands:     # execute builtins
+            commands[command](*command_with_arg[1:])    # passing the argument to the required command function
 
-        else: # executing commands in path
+        else:   # executing commands in path
             full_path = searcher(command)
             if full_path:
-                pid = os.fork() # creating a child process
+                pid = os.fork()     # creating a child process
 
-                if pid == 0: # entering child process
+                if pid == 0:    # entering child process
                     try:
                         os.execvp(command, command_with_arg)
-                    except OSError: # if the command cannot be executed
+                    except OSError:     # if the command cannot be executed
                         print(f"{command}: command not found", file=sys.stderr)
-                        os._exit(127) # exiting with an error
+                        os._exit(127)   # exiting with an error
 
-                else: # entering parent process
-                    os.waitpid(pid, 0) # parent needs to wait for the child process to end
+                else:   # entering parent process
+                    os.waitpid(pid, 0)  # parent needs to wait for the child process to end
 
             else:
                 print(f"{command}: command not found")    
